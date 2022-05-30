0c38e1f6e8f5822fa5cb8acb23edfeaec7303adb
Michael Halim <michaelhalimsenatra@gmail.com>
2022-05-30 11:46:41 +0700

    Added Storage and django-heroku

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -122,6 +124,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = 'static/'
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
@@ -134,3 +137,4 @@ MEDIA_URL  = '/media/'
 LOGIN_REDIRECT_URL = 'myapp:products'
 
 LOGIN_URL = 'users:login'
+django_heroku.settings(locals())
